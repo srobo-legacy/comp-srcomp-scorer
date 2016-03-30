a359330495bff54c313e31fd54a1bbd2d627d42c
Peter Law <PeterJCLaw@gmail.com>
2016-03-30 18:26:21 +0100

    Make authentication optional when launching module too
    Patch set 2: improve failure messages when exactly one of username
                 and password are provided.
    
    Change-Id: I78db131db5dca31dd5a7c47e19bac0357813638b

--- sr/comp/scorer/__main__.py
+++ sr/comp/scorer/__main__.py
@@ -8,13 +8,18 @@ if __name__ == '__main__':
                         help='Competition state git repository path')
     parser.add_argument('-l', '--local', action='store_true',
                         help='Disable fetch and push')
-    parser.add_argument('-u', '--username', help='Username', required=True)
-    parser.add_argument('-p', '--password', help='Password', required=True)
+    parser.add_argument('-u', '--username', help='Username')
+    parser.add_argument('-p', '--password', help='Password')
     args = parser.parse_args()
 
     app.config['COMPSTATE'] = args.compstate
     app.config['COMPSTATE_LOCAL'] = args.local
-    app.config['AUTH_USERNAME'] = args.username
-    app.config['AUTH_PASSWORD'] = args.password
+    if args.username is not None:
+        if args.password is None:
+            parser.error('--username requires --password')
+        app.config['AUTH_USERNAME'] = args.username
+        app.config['AUTH_PASSWORD'] = args.password
+    elif args.password is not None:
+        parser.error('--password requires --username')
 
     app.run(host='0.0.0.0', port=3000)
